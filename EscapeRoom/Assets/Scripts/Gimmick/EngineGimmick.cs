6900208b950ff242f6f941c27ce96f5ada2dd1d6
fuji250 <fuji8.goo@gmail.com>
2022-10-15 02:53:18 +0900

    Lighting set up.

---
+++ EscapeRoom/Assets/Scripts/Gimmick/EngineGimmick.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineGimmick : MonoBehaviour
+{
+    //ギミックを解除するアイテムを外で設定できるようにする
+    [SerializeField] Item.Type clearItem = default;
+
+    public void OnClickObj()
+    {
+        Debug.Log("s");
+        // アイテムCubeを持っているかどうか
+        bool Clear = ItemBox.instance.TryUseItem(clearItem);
+        if(Clear == true) // クリアアイテムを持っている場合
+        {
+            Debug.Log("ギミック解除");
+            gameObject.SetActive(false);
+        }
+    }
+}
